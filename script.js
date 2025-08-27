48580898d8ad42effc9fe5d52feabfce3587adfb
Mohammed- Hamim <mohammedhamim0158@gmail.com>
2025-08-28 01:51:25 +0600

    calling functionality is added

--- script.js
+++ script.js
@@ -13,58 +13,81 @@ document.getElementById('back-btn')
         document.getElementById('call-history').style.display = 'none'
     })
 
+// function for getting service name and number 
 
+function getServiceName(id) {
+    const serviceName = document.getElementById(id);
+    const serviceNameValue = serviceName.innerText;
+    return serviceNameValue;
+}
 
-function callNow(id) {
+// function for getting service helpline number
+function getServiceNum(id) {
+    const serviceNum = document.getElementById(id);
+    const serviceNumValue = serviceNum.innerText;
+    return serviceNumValue;
+}
+
+//
+
+// calling and alert function
+function callNow(id, name, num) {
+    const serviceName = getServiceName(name);
+    const serviceNumber = getServiceNum(num)
     const availableCoin = document.getElementById(id);
     const availableCoinValueNum = parseInt(availableCoin.innerText);
     if (availableCoinValueNum >= 20) {
-        alert('cost 20 coin for every call')
+        alert('📞 calling ' + serviceName + " " + serviceNumber)
     }
     else {
-        alert("not enough coin")
+        alert("You do not have enough coin. At least 20 coin needed for call")
         return;
     }
 
-    const newavailableCoinValueNum = availableCoinValueNum - 20;
-    document.getElementById('coin-number').innerText = newavailableCoinValueNum;
-    console.log(newavailableCoinValueNum)
+    const newAvailableCoinValueNum = availableCoinValueNum - 20;
+    document.getElementById('coin-number').innerText = newAvailableCoinValueNum;
+
 }
+
+
 
 // call button functionality
 document.getElementById('call-btn1')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name1', 'emergency-helpline');
     })
+
 document.getElementById('call-btn2')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name2', 'police-helpline')
     })
 document.getElementById('call-btn3')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name3', 'fire-service-helpline')
     })
 document.getElementById('call-btn4')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name4', 'ambulance-helpline')
     })
 document.getElementById('call-btn5')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name5', 'womenChild-helpline')
     })
 document.getElementById('call-btn6')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name6', 'anty-corruption-helpline')
     })
 document.getElementById('call-btn7')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name7', 'electricity-helpline')
     })
 document.getElementById('call-btn8')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name8', 'brac-helpline')
     })
 document.getElementById('call-btn9')
     .addEventListener('click', function () {
-        callNow('coin-number')
+        callNow('coin-number', 'service-name9', 'railway-helpline')
     })
+
+
